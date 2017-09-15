a19afcd808a2bb745c04357ff6cf2dbb426c11b2
kameron19921016 <kameron19921016@yahoo.com>
2017-09-15 09:13:30 +0800

    FWB: Add Volume Key Answer (1/2)
    Change-Id: Icd70db06699da3c81d7c596b4305a8384774c055
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4934,6 +4934,12 @@ public final class Settings {
         public static final String ENABLE_GAMING_MODE = "enable_gaming_mode";
 
         /**
+         * volume answer
+         * @hide
+         */
+        public static final String ANSWER_VOLUME_BUTTON_BEHAVIOR_ANSWER = "call_volume_answer";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
